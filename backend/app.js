d0a51b46e5d94411ebaa91f78d517247b2995614
mbukeRepo <mbuke404@gmail.com>
2022-06-24 01:53:28 +0200

    added implementation of github oath

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,77 @@
 const express = require("express");
+const session = require('express-session');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const Resource = require("./models/Resource");
 const path = require('path');
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require("passport");
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config({path: "./config.env"});
+const {CLIENTID, CLIENT_SECRET} = process.env;
 
+app.use(
+  session({
+    secret: "your secrete",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 app.use(express.json());
 app.use(cors());
+//app.use((req, res, next) => {
+//  console.log(req.user);
+//  next();
+//});
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  cb(null, id);
+});
+// auth middleware 
+passport.use(
+new GitHubStrategy({
+    clientID: CLIENTID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    cb(null, profile)
+  }
+)
+);
+
+app.get('/auth/github',(req, res, next) => {
+  console.log("authenticating...")
+  next();
+},
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', {
+    successRedirect: "http://localhost:3000/", 
+  })
+);
 
 // method GET
 // endpoint /resources
 // @desc: retrieves all resources
 app.get("/resources", async (req, res, next) => {
     let resources = await Resource.find();
+    console.log(req);
     if(req.query.search){
         resources = await Resource.find({$title:{$search: req.query.search}});
     }
