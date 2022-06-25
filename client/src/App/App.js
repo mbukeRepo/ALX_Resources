a4bf37a8834e70f585550fa371643f322004e334
mbukeRepo <mbuke404@gmail.com>
2022-06-25 22:55:26 +0200

    connected App component with state

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -7,25 +7,40 @@ import FeedList from "../pages/FEED-LIST";
 import SingleFeed from "../pages/SINGLE-FEED";
 import Auth from "../components/Auth/Auth"
 import "./App.css"
-const App = () => {
-return (
-    <div className="app-main__layout">
-        <Auth/>
-        <MainNav>
-            <Logo/>
-            <NavList/>
-        </MainNav>
-        <main >
-            <Switch>
-                <Route path="/add" component={AddFeed} exact/>
-                <Route path="/feed" component={FeedList}/>
-                <Route path="/:id" component={SingleFeed} exact/>
-                
-                <Route path="/" component={FeedList} exact/>
-            </Switch>
-        </main>
-    </div>
-);
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import {authenticate} from "../actions/authActionCreators"
+const App = (props) => {
+    useEffect(() => {
+        props.authenticate();
+    }, []);
+    return (
+        <div className="app-main__layout">
+            <Auth/>
+            <MainNav>
+                <Logo/>
+                <NavList/>
+            </MainNav>
+            <main >
+                <Switch>
+                    <Route path="/add" component={AddFeed} exact/>
+                    <Route path="/feed" component={FeedList}/>
+                    <Route path="/:id" component={SingleFeed} exact/>
+                    
+                    <Route path="/" component={FeedList} exact/>
+                </Switch>
+            </main>
+        </div>
+    );
 }
-
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        isAuth: state.auth.isAuth
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        authenticate: () => dispatch(authenticate())
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
