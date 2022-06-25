9a7a46fe7413d228c103ba8a570a402413ff2633
mbukeRepo <mbuke404@gmail.com>
2022-06-25 22:49:29 +0200

    connected app with redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App/App';
 import { BrowserRouter } from "react-router-dom";
 import Provider from "react-redux";
+import thunk from "redux-thunk";
+import reducer, {initialState} from "./reducers/rootReducer";
+import {createStore, compose, combineReducers, applyMiddleware} from "redux"
+
+const combineEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+const store = createStore(
+  combineReducers(reducer),
+  initialState,
+  combineEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
