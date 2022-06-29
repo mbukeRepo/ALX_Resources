c17aacdb9cdc2d148126a036ae2200a9fb555ff0
mbukeRepo <mbuke404@gmail.com>
2022-06-29 02:20:55 +0200

    reconfigured my card style

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,14 +5,16 @@ import "./Card.css"
 export default function Card(props) {
   return (
             <div className="feed-list__item" key={props.item._id}>
-                <Link to={"/" + props.item._id}>
-                    <p className="feed-list__item-title">{props.item.title}</p>
-                    <a className="owner" href='google.com'>
-                        <div className="owner-avatar"></div>
-                        <p className="owner-name">Mbuke Prince</p>
-                    </a>
+                    <Link to={"/" + props.item._id} className="feed-list__item-title">{props.item.title}</Link>
+                    <div className="owner" href='google.com'>
+                        <p>by</p>
+                        <a target="_blank" className="owner-name" rel="noreferrer" href="https://google.com">Mbuke Prince</a>
+                    </div>
+                    <div className='likes'>
+                        <div className="likes-icon"></div>
+                        <p>233 likes</p>
+                    </div>
                     <p className="feed-list__item-field">{props.item.field}</p>
-                </Link>
             </div>
     )
 }
