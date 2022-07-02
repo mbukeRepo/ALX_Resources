c5097fc5802db6a548f172516fb660c3ff420316
mbukeRepo <mbuke404@gmail.com>
2022-07-02 15:11:56 +0200

    reconfigured the search components

--- client/src/Search/Search.js
+++ client/src/Search/Search.js
@@ -1,39 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./Search.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faHeart, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Search(props) {
-  const onChange = (e) => {
-    props.search(e.target.value);
-  } 
-
+  const onKeyPress = (e) => {
+    if(e.key === "Enter"){
+      console.log("enter pressed");
+    }
+  };
   return (
     <div className='search-container'>
         <div className="search-section">
                 <FontAwesomeIcon icon={faSearch} />
-                <input type="text" onChange={onChange}  />
-        </div>
-        <div className="tags-section">
-            <div className="tag react" onClick={() => props.search("react", "tag")}>
-              <p>React</p>
-            </div>
-            <div className="tag python" onClick={() => props.search("python", "tag")}>
-              <p>Python</p>
-            </div>
-            <div className="tag css" onClick={() => props.search("css", "tag")}>
-              <p>CSS</p>
-            </div>
-            <div className="tag mysql" onClick={() => props.search("mysql", "tag")}>
-              <p>Mysql</p>
-            </div>
-             <div className="tag flask" onClick={() => props.search("flask", "tag")}>
-              <p>Flask</p>
-            </div>
-            <div className="tag c" onClick={() => props.search("c", "tag")}>
-              <p>Flask</p>
-            </div>
+                <input type="text" onKeyPress={onKeyPress}/>
         </div>
     </div>
   )
--- client/src/components/Nav/NavList.js
+++ client/src/components/Nav/NavList.js
@@ -6,22 +6,26 @@ const NavList = props => {
     return (
         <ul className="main-nav__list">
             {props.isAuth ?
-            <Fragment>
                 <li className="main-nav__list-item add">
                     <Link to="/add">+</Link>
                 </li>
-                <li className="main-nav__list-item avatar">
-                    <div className="user-avatar">
-                        <img src={props.photo} alt="" srcset="" />
-                    </div>
-                    <p>{props.userName}</p>
-                </li>
-            </Fragment>
+                
                 : null
             } 
-            
-                
-               
+            <li className="main-nav__list-item avatar">
+                {props.isAuth ? 
+                    <Fragment>
+                        <div className="user-avatar">
+                            <img src={props.photo} alt="" srcset="" />
+                        </div>
+                        <p>{props.userName}</p>
+                    </Fragment>
+                    
+                    : null
+                }
+                    
+                    
+            </li>
         </ul>
     );
 }
